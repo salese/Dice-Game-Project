d7e581e39f867c6c86a392418043ab64405304fd
Alexander Atuh <50431402+salese@users.noreply.github.com>
2021-01-28 21:26:04 -0700

    Changed code to say Refresh if it's a tie

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1
+var randomNumber2 = Math.floor(Math.random() * 6) + 1
 
-var dice1 = document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice"+randomNumber+".png");
-var dice2 = document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice"+randomNumber2+".png");
+var dice1 = document.querySelector('.img1').setAttribute('src', 'images/dice'+randomNumber1+'.png');
+var dice2 = document.querySelector('.img2').setAttribute('src', 'images/dice'+randomNumber2+'.png');
 
-if(randomNumber > randomNumber2){
-  document.querySelector("h1").textContent = "😁 Player 1 Wins";
-} else if(randomNumber < randomNumber2){
-  document.querySelector("h1").textContent = "Player 2 Wins 😁";
-} else if(randomNumber === randomNumber2){
-  document.querySelector("h1").textContent = "Draw 😱";
+if(randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins";
+} else if(randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = "Player 2 Wins 🚩";
 }
